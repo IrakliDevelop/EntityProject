bd05ec4bd7931dfcd9010c607dfcfcd83e6f76a6
IrakliDevelop <i_iremashvili@cu.edu.ge>
2019-06-12 22:46:18 +0400

    style refactoring

--- IrakliIremashviliEntityProject/Program.cs
+++ IrakliIremashviliEntityProject/Program.cs
@@ -6,55 +6,60 @@ using System.Threading.Tasks;
 
 namespace IrakliIremashviliEntityProject
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             using (var db = new DemoDbContext())
             {
                 Console.WriteLine("Welcome to my Entity Demo Program. \nSelect the index of desirable action");
-                bool exit = false;
-                
+                var exit = false;
+
                 IList<Phone> phones = new List<Phone>();
                 IList<Manufacturer> manufacturers = new List<Manufacturer>();
                 IList<OS> oss = new List<OS>();
-                
+
                 while (!exit)
                 {
                     Console.WriteLine("1)Show All Available Phones\n2)Add new phone");
-                    var action = Int32.Parse(Console.ReadLine());
+                    var action = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
 
-                    if (action == 1)
+                    switch (action)
                     {
-                        foreach (var phone in db.Phones)
+                        case 1:
                         {
-                            Console.WriteLine(phone.Model);
+                            foreach (var phone in db.Phones)
+                            {
+                                Console.WriteLine(phone.Model);
+                            }
+
+                            Console.WriteLine("That's All");
+                            break;
                         }
 
-                        Console.WriteLine("That's All");
-                    } else if (action == 2)
-                    {
-                        Console.WriteLine("Enter Phone Model's Name: ");
-                        var model = Console.ReadLine();
-                        Console.WriteLine("Select the index of Manufacturer: ");
-                        var i = 0;
-                        foreach (var man in db.Manufacturers)
+                        case 2:
                         {
-                            i++;
-                            Console.WriteLine(i + ") " + man.Name);
+                            Console.WriteLine("Enter Phone Model's Name: ");
+                            var model = Console.ReadLine();
+                            Console.WriteLine("Select the index of Manufacturer: ");
+                            var i = 0;
+                            foreach (var man in db.Manufacturers)
+                            {
+                                i++;
+                                Console.WriteLine(i + ") " + man.Name);
+                            }
 
+                            var manIndex = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+                            var chosenManufacturer = db.Manufacturers.ToArray();
+                            phones.Add(new Phone {Model = model, Manufacturer = chosenManufacturer[manIndex - 1]});
+                            break;
                         }
-                        var manIndex = Int32.Parse(Console.ReadLine());
-                        var chosenManufacturer = db.Manufacturers.ToArray();
-                        phones.Add(new Phone() { Model = model, Manufacturer = chosenManufacturer[manIndex-1], });
-                        
-                    } else if (action == 3)
-                    {
-                        db.Phones.AddRange(phones);
-                        db.SaveChanges();
-                        exit = true;
+                        case 3:
+                            db.Phones.AddRange(phones);
+                            db.SaveChanges();
+                            exit = true;
+                            break;
                     }
-                
                 }
             }
         }
